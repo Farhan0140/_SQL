b6c450ea42f1062cd6df4a0dc15f698876177c37
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-18 17:38:00 +0600

    Create 10_Revisiting_Foreign_Key.sql

---
+++ 10_Revisiting_Foreign_Key.sql
@@ -0,0 +1,39 @@
+CREATE TABLE dept (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO dept (id, dept_name)
+VALUES
+    (101, "Science"),
+    (102, "English"),
+    (103, "BBA"),
+    (104, "CSE")
+
+SELECT * FROM dept;
+
+---->
+UPDATE dept
+SET id = 169        -----> It updates the  teacher table too,,  Because of ON UPDATE CASCADE & ON DELETE CASCADE
+WHERE id = 104;                                         ----> If we don't use UPDATE CASCADE & ON DELETE CASCADE nothing change on Teacher Table
+
+
+------------------------------------> FOREIGN KEY
+
+CREATE TABLE teacher (
+    id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    dept_id INT,
+    FOREIGN KEY(dept_id) REFERENCES dept(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+INSERT INTO teacher (id, name, dept_id)
+VALUES
+    (101, "BOB", 102),
+    (102, "ADAM", 102),
+    (103, "CASY", 101),
+    (104, "RABBI", 104)
+
+SELECT * FROM teacher;
