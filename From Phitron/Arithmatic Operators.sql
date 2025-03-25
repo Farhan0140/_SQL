2e7a032618356323b10af7b6f24d394e5ac0c57a
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-25 23:17:40 +0600

    Refactor student table to include CSE course marks and update related SQL queries

--- From Phitron/Arithmatic Operators.sql
+++ From Phitron/Arithmatic Operators.sql
@@ -4,30 +4,33 @@ CREATE TABLE student(
 
     Roll CHAR(4),
     Name VARCHAR(50),
-    Email VARCHAR(50),
-    Address VARCHAR(255),
-    Age INT,
+    CSE_101 INT DEFAULT(0),
+    CSE_102 INT DEFAULT(0),
+    CSE_103 INT DEFAULT(0),
     CGPA FLOAT NOT NULL,
 
-    Constraint pk_rule PRIMARY KEY(Roll),
-    Constraint unique_rule UNIQUE(Email),
-    Constraint checking_rule CHECK(Age > 10)
+    Constraint pk_rule PRIMARY KEY(Roll)
 
 );
 
-INSERT INTO student (Roll, Name, Email, Address, Age, CGPA)
+INSERT INTO student (Roll, Name, CSE_101, CSE_102, CSE_103, CGPA)
 VALUES 
-    ('0001', 'Farhan', 'abc@gmail.com', 'Dhaka', 23, 3.45),
-    ('0002', 'Nadim', 'abcd@gmail.com', 'Aka', 25, 2.45),
-    ('0003', 'Ruby Franklin', 'tohuho@go.ag', 'Equatorial Guinea', 25, 3.85),
-    ('0004', 'Viola Cross', 'ake@ezafocu.cv', 'Grenada', 29, 4.00);
+    ('0001', 'Farhan', 78, 89, 57, 3.14),
+    ('0002', 'Dora', 87, 71, 90, 3.39),
+    ('0003', 'Julian', 51, 74, 40, 3.62),
+    ('0004', 'Francisco', 89, 72, 15, 2.31);
 
 
 
 
-SELECT CGPA + Age FROM student
+
+SELECT (CSE_101 + CSE_102 + CSE_103) AS Total_Marks
+FROM student
 WHERE Roll = '0001';
 
 
-SELECT CGPA * 2 FROM student
+SELECT (CSE_101 + CSE_102 + CSE_103)/3 AS Average_Marks
+FROM student
 WHERE Roll = '0004';
+
+
