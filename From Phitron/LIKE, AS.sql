b7926d912777454a46ad6781c19ba611fa809f74
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-26 06:20:04 +0600

    Add SQL script for employee details with LIKE and AS examples

---
+++ From Phitron/LIKE, AS.sql
@@ -0,0 +1,43 @@
+CREATE TABLE employee_details(
+
+     emp_id VARCHAR(8),
+     emp_name VARCHAR(255),
+     emp_designation VARCHAR(20),
+     emp_age INT
+
+);
+
+INSERT INTO employee_details 
+VALUES
+    ('E40001','Samne PAVAN','H.R',36),
+    ('E40002','PAVAN Picone Kicu','MANAGER',28),
+    ('E40003','PAVAN KUMAR','ASST MANAGER',28),
+    ('E40004','Tarek','ASST MANAGER',25),
+    ('E40005','Samne PAVAN Picone Kicu Ace','GENERAL MANAGER',26);
+
+
+
+-- For Both Side
+
+SELECT * FROM employee_details
+WHERE emp_name LIKE '%PAVAN%';
+
+
+-- For Left Side
+
+SELECT * FROM employee_details
+WHERE emp_name LIKE '%PAVAN';
+
+
+-- For Right Side
+SELECT * FROM employee_details
+WHERE emp_name LIKE 'PAVAN%';
+
+
+
+-- AS
+SELECT emp_name AS Name -- Showing emp_name as a Name in a column name
+FROM employee_details;
+
+
+--
