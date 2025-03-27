8dc33b9836c57a581ed63c2ae1430eec2bf1af49
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-28 01:43:52 +0600

    Add SQL queries for employee management and salary analysis in Join.sql

--- From Phitron/Join.sql
+++ From Phitron/Join.sql
@@ -91,4 +91,42 @@ FROM employees AS emp
 
 
 
---
+-- Write a SQL query to retrieve a list of employees along with the first name of their respective managers.
+SELECT mana.first_name , emp.first_name AS manager_name
+FROM employees AS emp
+    JOIN employees AS mana
+    ON emp.employee_id = mana.manager_id; 
+
+
+
+-- A company wants to identify departments that do not have any employees assigned to them
+SELECT departments.department_name
+FROM departments
+    LEFT JOIN employees
+    ON departments.department_id = employees.department_id
+WHERE employees.department_id IS NULL;
+
+
+
+--Each employee's first name. Their salary. The difference between their salary and the average salary of their department.
+SELECT 
+    employees.first_name, 
+    employees.salary,
+    (
+        SELECT AVG(salary) 
+        FROM employees
+        WHERE department_id = employees.department_id
+    ) - employees.salary
+FROM employees
+    JOIN departments
+    ON employees.department_id = departments.department_id;
+
+
+
+-- Write an SQL query to retrieve the minimum salary in each department, ensuring that only departments where the minimum salary is greater than $5000 are included in the result.
+SELECT departments.department_name
+FROM employees
+    JOIN departments
+    ON departments.department_id = employees.department_id
+GROUP BY departments.department_name
+HAVING MIN(salary) > 5000;
