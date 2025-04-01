3aac129426a120a912f7f3058f3bc417dbebce60
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-04-01 20:21:44 +0600

    Add Control Flow Functions SQL examples

---
+++ From Phitron/Control Flow Functions.sql
@@ -0,0 +1,28 @@
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    WHEN conditionN THEN resultN
+    ELSE result
+END;
+
+
+
+--
+SELECT OrderID, Quantity,
+CASE
+    WHEN Quantity > 30 THEN 'The quantity is greater than 30'
+    WHEN Quantity = 30 THEN 'The quantity is 30'
+    ELSE 'The quantity is under 30'
+END AS QuantityText
+FROM OrderDetails;
+
+
+
+-- if
+SELECT IF(500<1000, 5, 10);
+
+
+
+
+
+--
